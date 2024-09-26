28ae9e3519afc5bd07067f9725e8f377f7059d27
Stanley Zheng <szheng31@stuy.edu>
2024-09-26 12:10:55 -0500

    fixed fonts not loading for education tab

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,173 +1,162 @@
-"use client"
+"use client";
+
 import VinylImage from "../components/VinylImage";
-import bridgingseas from "../imgs/bridgingseas.png"
-import vanderbilt from "../imgs/Dimensional V.png"
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import Typography from '@mui/material/Typography';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import vanderbilt from "../imgs/Dimensional V.png";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem, { timelineItemClasses } from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import Typography from "@mui/material/Typography";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   typography: {
     allVariants: {
-      fontFamily: 'JetBrainsMono',
-      textTransform: 'none',
+      fontFamily: "JetBrainsMono",
+      textTransform: "none",
+    },
+    h1: {
+      fontSize: "1.875rem", // Base font size (text-3xl)
+      "@media (min-width:640px)": {
+        fontSize: "3rem", // Adjust font size to 5xl on screens >= 640px (sm)
+      },
     },
   },
 });
 
 export default function Education() {
-    
+  return (
+    <div className="grid md:grid-cols-2 md:grid-rows-1 grid-rows-2 pt-3 bg-gradient-to-l from-[#BDA567] to-[#FFFDD0] min-h-screen w-full">
+      <div>
+        <p className="animate-slide-in sm:text-9xl text-8xl">Education</p>
 
-    return (
-        <div className="grid md:grid-cols-2 md:grid-rows-1 grid-rows-2 pt-3 bg-gradient-to-l from-[#BDA567] to-[#FFFDD0]  min-h-screen w-full">
-            <div>
-                
-                <p className="animate-slide-in sm:text-9xl text-8xl">Education</p>
-                <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Timeline
+            sx={{
+              [`& .${timelineItemClasses.root}:before`]: {
+                flex: 0,
+                padding: 0,
+              },
+            }}
+          >
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot
+                  sx={{
+                    backgroundColor: "black", // Apply styles directly to TimelineDot
+                    width: "16px",
+                    height: "16px",
+                  }}
+                />
+                <TimelineConnector sx={{ backgroundColor: "black" }} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography variant="h1">Stuyvesant High School</Typography>
+                <Typography>Bridging Seas IT Director</Typography>
+                <Typography>Intro to CS TA</Typography>
+                <Typography>CS Dojo Tutor</Typography>
+              </TimelineContent>
+            </TimelineItem>
 
-                <Timeline sx={{
-                        [`& .${timelineItemClasses.root}:before`]: {
-                            flex: 0,
-                            padding: 0,
-                        },
-                    }}>
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot className="bg-black" />
-                                <TimelineConnector className="bg-black" />
-                            </TimelineSeparator>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot
+                  sx={{
+                    backgroundColor: "black", // Apply styles directly to TimelineDot
+                    width: "16px",
+                    height: "16px",
+                  }}
+                />
+                <TimelineConnector sx={{ backgroundColor: "black" }} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography variant="h1">Columbia Pre-College</Typography>
+                <Typography>
+                  MATLAB Programming and Mathematical Modeling
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
 
-                            <TimelineContent className="font-[JetBrainsMono]">
-                                <Typography className="font-[JetBrainsMono] sm:text-5xl text-3xl">
-                                    Stuyvesant High School
-                                </Typography>
-                                <Typography className="font-[JetBrainsMono]">
-                                    Bridging Seas IT Director
-                                    </Typography>
-                                    <Typography className="font-[JetBrainsMono]">
-                                    Intro to CS TA
-                                    </Typography>
-                                    <Typography className="font-[JetBrainsMono]">
-                                    CS Dojo Tutor
-                                    </Typography>
-                            </TimelineContent>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot
+                  sx={{
+                    backgroundColor: "black", // Apply styles directly to TimelineDot
+                    width: "16px",
+                    height: "16px",
+                  }}
+                />
+                <TimelineConnector sx={{ backgroundColor: "black" }} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography variant="h1">theCoderSchool</Typography>
+                <Typography>Code Coach</Typography>
+              </TimelineContent>
+            </TimelineItem>
 
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot className="bg-black" />
-                                <TimelineConnector className="bg-black" />
-                            </TimelineSeparator>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot
+                  sx={{
+                    backgroundColor: "black", // Apply styles directly to TimelineDot
+                    width: "16px",
+                    height: "16px",
+                  }}
+                />
+                <TimelineConnector sx={{ backgroundColor: "black" }} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography variant="h1">Chyron</Typography>
+                <Typography>Consulting Intern</Typography>
+              </TimelineContent>
+            </TimelineItem>
 
-                            <TimelineContent className="font-[JetBrainsMono]">
-                                <Typography className="font-[JetBrainsMono] sm:text-5xl text-3xl">
-                                    Columbia Pre-College
-                                </Typography>
-                                <Typography className="font-[JetBrainsMono]">
-                                    MATLAB Programming and Mathematical Modeling
-                                    </Typography>
-                            </TimelineContent>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot
+                  sx={{
+                    backgroundColor: "black", // Apply styles directly to TimelineDot
+                    width: "16px",
+                    height: "16px",
+                  }}
+                />
+                <TimelineConnector sx={{ backgroundColor: "black" }} />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography variant="h1">Vanderbilt University</Typography>
+                <Typography>Computer Science and Applied Math Major</Typography>
+                <Typography>Electrical/Computer Engineering Minor</Typography>
+                <Typography>VandyHacks Development Team Member</Typography>
+                <Typography>
+                  Vanderbilt Ultimate Frisbee Recruitment Chair
+                </Typography>
+              </TimelineContent>
+            </TimelineItem>
 
-                        </TimelineItem>
-                        
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot className="bg-black" />
-                                <TimelineConnector className="bg-black" />
-                            </TimelineSeparator>
+            <TimelineItem>
+              <TimelineSeparator>
+                <TimelineDot
+                  sx={{
+                    backgroundColor: "black", // Apply styles directly to TimelineDot
+                    width: "16px",
+                    height: "16px",
+                  }}
+                />
+              </TimelineSeparator>
+              <TimelineContent>
+                <Typography variant="h1">REIDD</Typography>
+                <Typography>Software Engineering Intern</Typography>
+              </TimelineContent>
+            </TimelineItem>
+          </Timeline>
+        </ThemeProvider>
+      </div>
 
-                            <TimelineContent className="font-[JetBrainsMono]">
-                                <Typography className="font-[JetBrainsMono] sm:text-5xl text-3xl">
-                                    theCoderSchool
-                                </Typography>
-                                <Typography className="font-[JetBrainsMono]">
-                                    Code Coach
-                                    </Typography>
-                            </TimelineContent>
-
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot className="bg-black" />
-                                <TimelineConnector className="bg-black "  />
-                            </TimelineSeparator>
-
-                            <TimelineContent className="font-[JetBrainsMono]">
-                                <Typography className="font-[JetBrainsMono] sm:text-5xl text-3xl">
-                                    Chyron
-                                </Typography>
-                                <Typography className="font-[JetBrainsMono] ">
-                                    Consulting Intern
-                                    </Typography>
-                            </TimelineContent>
-
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot className="bg-black" />
-                                <TimelineConnector className="bg-black" />
-                            </TimelineSeparator>
-
-                            <TimelineContent className="font-[JetBrainsMono]">
-                                <Typography className="font-[JetBrainsMono] sm:text-5xl text-3xl">
-                                    Vanderbilt University
-                                </Typography>
-                                <Typography className="font-[JetBrainsMono]">
-                                    Computer Science and Applied Math Major
-                                    </Typography>
-                                    <Typography className="font-[JetBrainsMono]">
-                                    Electrical/Computer Engineering Minor
-                                    </Typography>
-                                    <Typography className="font-[JetBrainsMono]">
-                                    VandyHacks Development Team Member
-                                    </Typography>
-                                    <Typography className="font-[JetBrainsMono]">
-                                    Vanderbilt Ultimate Frisbee Recruitment Chair
-                                    </Typography>
-                            </TimelineContent>
-
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineSeparator>
-                                <TimelineDot className="bg-black" />
-                                
-                            </TimelineSeparator>
-
-                            <TimelineContent className="font-[JetBrainsMono]">
-                                <Typography className="font-[JetBrainsMono] sm:text-5xl text-3xl">
-                                    REIDD
-                                </Typography>
-                                <Typography className="font-[JetBrainsMono]">
-                                    Software Engineering Intern
-                                    </Typography>
-                            </TimelineContent>
-
-                        </TimelineItem>
-                        
-                        
-                    </Timeline>
-                    </ThemeProvider>
-
-                
-                </div>
-                
-            
-            
-            
-
-            <div  className="flex justify-items-center animate-top-down">
-                <VinylImage imageSrc={vanderbilt.src} link="/" spin={true} />
-            </div>
-            
-        </div>
-
-
-
-    );
+      <div className="flex justify-items-center animate-top-down">
+        <VinylImage imageSrc={vanderbilt.src} link="/" spin={true} />
+      </div>
+    </div>
+  );
 }
-
